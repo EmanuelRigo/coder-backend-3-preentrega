4e9063b82edafca851e55365887136eb468ea21f
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-02-04 22:31:04 -0300

    adding

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getMockingPets, getMockingUsers, getMockingUsersByNumber } from "../controllers/mocks.controller.js";
+import { getMockingPets, getMockingUsers} from "../controllers/mocks.controller.js";
 
 const router = Router();
 
 
 router.get('/mockingpets/:num', getMockingPets);
-router.get('/mockingusers/:num', getMockingUsersByNumber);
+router.get('/mockingusers/:num', getMockingUsers);
 
 export default router;
