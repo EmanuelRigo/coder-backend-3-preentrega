85929948b22d88999ae4e5184354f52db83d5a56
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-02-26 21:34:18 -0300

    add test

--- test/adoptions.test.js
+++ test/adoptions.test.js
@@ -4,10 +4,18 @@ const requester = supertest("http://localhost:8080");
 
 describe("Testing de adoption", () => {
     describe("Testing del Router de adoptions", () => {
+
         it("Endpoint GET /api/adoptions debe traer todas las adoptiones", async () => {
-            const { status, body } = await requester.get("/api/adoptions");
+            const { status, _body } = await requester.get("/api/adoptions");
             expect(status).to.equal(200);
-            expect(body.payload).to.be.an("array");
+            expect(_body.payload).to.be.an("array");
         });
+
+        it("traamos una adopcion por id", async () => {
+            let idAdoption = "67bf8fbcdca6273dc312ca7f";
+            const { status, _body } = await requester.get(`/api/adoptions/${idAdoption}`);
+            expect(status).to.equal(200);
+            expect(_body.payload).to.be.have.property("_id").that.equal(idAdoption);
+        })
     });
 });
